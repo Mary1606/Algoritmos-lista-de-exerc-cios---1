1b39e165fcf45e36f62e32d8991fc69a4ee707a1
Mary1606 <79229663+Mary1606@users.noreply.github.com>
2026-08-10 09:11:07 -0300

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -1,10 +1,13 @@
+#Usuário digita um número
 n = int(input('Digite um número: '))
 
+#Função que determina o somatorio dos número 1 até o n
 def somatorio(numero):
     soma = 0
     for i in range(1,numero +1):
         soma += i
     return soma
 
+#Imprimir na tela o resultado
 resultado = somatorio(n)
 print(f'Seu resultado é {resultado}')
