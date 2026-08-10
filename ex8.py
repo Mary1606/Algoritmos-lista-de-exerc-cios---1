f62673f69c3c5f534a5551efb25297bb015ff0c0
Mary1606 <79229663+Mary1606@users.noreply.github.com>
2026-08-10 09:14:30 -0300

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,4 +1,5 @@
 soma = 0
+#Usuário escolhe um número e só para quando digitar: 0
 while True:
     numeros = int(input('Digite um número ou digite 0 para parar: '))
     soma += numeros
@@ -6,4 +7,6 @@ while True:
         continue
     else:
         break
+
+#Imprimir na tela o resultado
 print(f'\nA soma dos números é igual a: {soma}')
