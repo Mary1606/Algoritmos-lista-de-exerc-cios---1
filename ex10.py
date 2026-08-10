39dc9dccb1ac2dc8af3e9e33f764c2e549bde8fc
Mary1606 <79229663+Mary1606@users.noreply.github.com>
2026-08-10 09:16:31 -0300

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,10 +1,14 @@
+#Lista de números escolhidos pelo usuário
 numeros = []
 
+#Usuário escolhe os números e eles são adicionados a uma lista
 for i in range(1,6):
     numero = int(input(f'Digite o {i}º número: '))
     numeros.append(numero)
 
+#Determinam qual o maior e o menor número dentro da lista
 maior = max(numeros)
 menor = min(numeros)
 
+#Imprimir na tela o resultado 
 print(f'Maior número é {maior} \nMenor número é {menor}')
