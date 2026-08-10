7ebbb9f9e5cd9c79229bc768dcb8c83971bee858
Mary1606 <79229663+Mary1606@users.noreply.github.com>
2026-08-10 09:13:15 -0300

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,10 +1,14 @@
+#Lista dos números que o usuário escolheu
 numeros = []
 
+#Usuário escolhe os números e são adicionados a lista
 for i in range(1,11):
     numero = int(input(f'Digite o {i}º número: '))
     numeros.append(numero)
 
+#Soma e média dos números da lista
 soma = sum(numeros)
 media = round((soma / 10),2)
 
+#Imprimir na tela o resultado
 print(f'\nA soma dos números digitados é: {soma} \nA média deles é: {media}')
