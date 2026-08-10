97054a54f1e5e8c75c3692d4f73a096365133823
Mary1606 <79229663+Mary1606@users.noreply.github.com>
2026-08-10 09:09:13 -0300

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,5 +1,7 @@
+#Usuário digita sua nota
 nota = float(input('Digite sua nota: '))
 
+#Função que determina se ele foi aprovado ou não. Acima de 7 pontos*
 def nota_final(nota):
     if nota >= 7:
         print('Você foi aprovado!')
